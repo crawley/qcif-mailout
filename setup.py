d4a4fdea5de4759dbe4187b7d0fb376b2161e132
Stephen Crawley <crawley.stephen@gmail.com>
2016-01-30 15:14:11 +1000

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='qcif-mainout',
+      version='0.1',
+      description='The QCIF Ops mailout tool',
+      url='http://github.com/crawley/qcif-mailout',
+      author='Stephen Crawley',
+      author_email='s.crawley@uq.edu.au',
+      license='MIT',
+      packages=['mailout'],
+      zip_safe=False)
